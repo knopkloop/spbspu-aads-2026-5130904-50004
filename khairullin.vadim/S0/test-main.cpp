ade77340bedf15cf2a64371e5224d2fecc5e4edc
BinaryKenny <khairullinvadim2006@gmail.com>
2026-02-06 18:58:52 +0300

    I added the test

--- khairullin.vadim/S0/test-main.cpp
+++ khairullin.vadim/S0/test-main.cpp
@@ -1,9 +1,5 @@
 #define BOOST_TEST_MODULE S0
 #include <boost/test/included/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(output_test)
-{
-  int i = 1;
-  BOOST_TEST(i);
-  BOOST_TEST(i == 2);
-}
+BOOST_AUTO_TEST_CASE(test)
+{}
