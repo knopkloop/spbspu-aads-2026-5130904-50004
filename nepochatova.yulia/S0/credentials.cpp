0ab44dafb1391b61e65170ac91f34251f25f8d45
GenJune <julia.nepochatova@gmail.com>
2026-02-08 22:06:49 +0300

    out_credentials implementation

---
+++ nepochatova.yulia/S0/credentials.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "credentials.hpp"
+
+std::ostream & nepochatova::out_credentials(std::ostream & out)
+{
+  return out << "nepochatova.yulia";
+}
--- nepochatova.yulia/S0/main.cpp
+++ nepochatova.yulia/S0/main.cpp
@@ -1,1 +1,7 @@
-int main(){}
+#include <iostream>
+#include "credentials.hpp"
+
+int main()
+{
+  nepochatova::out_credentials(std::cout) << "\n";
+}
