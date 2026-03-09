27ff4b26fd03d0c4f16e26b2cad4363d8432674e
knopkloop <hruslan2007@gmail.com>
2026-03-09 18:56:00 +0300

    added read input

--- haliullin.ruslan/S1/main.cpp
+++ haliullin.ruslan/S1/main.cpp
@@ -9,9 +9,26 @@ int main()
 {
    try
   {
-    BiList<std::pair<std::string, BiList<unsigned long long>>> storage;
+    BiList<std::pair<std::string, BiList<unsigned long long>>> stor;
     std::string token;
     unsigned long long value = 0;
+
+    while (std::cin >> token)
+    {
+      BiList<unsigned long long> numbers;
+
+      while (std::cin >> value)
+      {
+        numbers.push_back(value);
+      }
+
+      stor.push_back(std::make_pair(token, std::move(numbers)));
+
+      if (!std::cin.eof())
+      {
+        std::cin.clear();
+      }
+    }
   }
   catch(const std::exception &e)
   {
