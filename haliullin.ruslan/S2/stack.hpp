8ad2185527b6ac1990b57d4db7d44ce2f8856966
knopkloop <hruslan2007@gmail.com>
2026-03-23 00:56:13 +0300

    added stack realization

--- haliullin.ruslan/S2/stack.hpp
+++ haliullin.ruslan/S2/stack.hpp
@@ -21,6 +21,62 @@ namespace haliullin
     size_t size() const;
     void clear();
   };
+
+  template < class T >
+  void Stack< T >::push(const T & rhs)
+  {
+    data.push_back(rhs);
+  }
+
+  template < class T >
+  T Stack< T >::drop()
+  {
+    if (data.is_empty())
+    {
+      throw std::runtime_error("Stack is empty");
+    }
+    T val = data.back();
+    data.pop_back();
+    return val;
+  }
+
+  template < class T >
+  T & Stack< T >::top()
+  {
+    if (data.is_empty())
+    {
+      throw std::runtime_error("Stack is empty");
+    }
+    return data.back();
+  }
+
+  template < class T >
+  const T & Stack< T >::top() const
+  {
+    if (data.is_empty())
+    {
+      throw std::runtime_error("Stack is empty");
+    }
+    return data.back();
+  }
+
+  template < class T >
+  bool Stack< T >::is_empty() const
+  {
+    return data.is_empty();
+  }
+
+  template < class T >
+  size_t Stack< T >::size() const
+  {
+    return data.getsize();
+  }
+
+  template < class T >
+  void Stack< T >::clear()
+  {
+    data.clear();
+  }
 }
 
 #endif
