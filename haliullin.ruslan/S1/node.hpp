bc038102234887d5e287fc3a82bd43f9acaab55a
knopkloop <hruslan2007@gmail.com>
2026-03-23 01:17:00 +0300

    done beautifully

--- haliullin.ruslan/S1/node.hpp
+++ haliullin.ruslan/S1/node.hpp
@@ -1,19 +1,20 @@
 #ifndef NODE_HPP
 #define NODE_HPP
-#include <iostream>
+
+#include <utility>
 
 namespace haliullin
 {
-  template< class T > class BiList;
-  template< class T > class LIter;
-  template< class T > class LCIter;
+  template < class T > class BiList;
+  template < class T > class LIter;
+  template < class T > class LCIter;
 
   template< class T >
   struct Node
   {
     T val;
-    Node *prev;
-    Node *next;
+    Node * prev;
+    Node * next;
 
     Node(const T & value, Node * p = nullptr, Node * n = nullptr):
       val(value),
