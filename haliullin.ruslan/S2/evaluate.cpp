2b3492c384081adbe0b0422df624cb3538a29319
knopkloop <hruslan2007@gmail.com>
2026-03-23 18:59:50 +0300

    added subfunctions isNumber and isOperator for token

--- haliullin.ruslan/S2/evaluate.cpp
+++ haliullin.ruslan/S2/evaluate.cpp
@@ -23,12 +23,80 @@ int haliullin::get_priority(const std::string & oper)
 
 long long haliullin::calculate(long long a, long long b, const std::string & oper)
 {
-  if (oper == "+") return sum(a, b);
-  else if (oper == "-") return subtract(a, b);
-  else if (oper == "*") return multiply(a, b);
-  else if (oper == "/") return divide(a, b);
-  else if (oper == "%") return mod(a, b);
-  else if (oper == "lcm") return lcm(a, b);
+  if (oper == "+")
+  {
+    return sum(a, b);
+  }
+  else if (oper == "-")
+  {
+    return subtract(a, b);
+  }
+  else if (oper == "*")
+  {
+    return multiply(a, b);
+  }
+  else if (oper == "/")
+  {
+    return divide(a, b);
+  }
+  else if (oper == "%")
+  {
+    return mod(a, b);
+  }
+  else if (oper == "lcm")
+  {
+    return lcm(a, b);
+  }
+
   throw std::logic_error("invalid operator");
 }
 
+bool haliullin::isOperator(const std::string & token)
+{
+  if (token.empty())
+  {
+    return false;
+  }
+
+  if (token.length() == 1 && ((token[0] == '-') || (token[0] == '+') || (token[0] == '/') || (token[0] == '*') || (token[0] == '%')))
+  {
+    return true;
+  }
+  else if (token.length() == 3)
+  {
+    std::string tz = "lcm";
+
+    if (token == tz)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool isNumber(const std::string & token)
+{
+  if (token.empty())
+  {
+    return false;
+  }
+
+  size_t st = 0;
+  if (token[st] == '-')
+  {
+    if (token.length() == 1)
+    {
+      return false;
+    }
+    ++st;
+  }
+
+  for (size_t i = st; i < token.length(); ++i)
+  {
+    if (!std::isdigit(static_cast<unsigned char>(token[i])))
+    {
+      return false;
+    }
+  }
+  return true;
+}
--- haliullin.ruslan/S2/evaluate.hpp
+++ haliullin.ruslan/S2/evaluate.hpp
@@ -2,11 +2,16 @@
 #define EVALUATE_HPP
 
 #include "stack.hpp"
+#include "queue.hpp"
 
 namespace haliullin
 {
   int get_priority(const std::string & oper);
   long long calculate(long long a, long long b, const std::string & oper);
+
+  bool isOperator(const std::string & token);
+  bool isNumber(const std::string & token);
+
 }
 
 #endif
