d55257cb81b63a031890b0dd57fa978e91e57f90
knopkloop <hruslan2007@gmail.com>
2026-05-26 18:51:22 +0300

    fix unit

--- haliullin.ruslan/S2/test-queue.cpp
+++ haliullin.ruslan/S2/test-queue.cpp
@@ -4,43 +4,79 @@
 
 BOOST_AUTO_TEST_SUITE(QueueTests)
 
-BOOST_AUTO_TEST_CASE(PushAndPop)
+BOOST_AUTO_TEST_CASE(push)
 {
   haliullin::Queue< int > queue;
   BOOST_CHECK(queue.is_empty());
-  queue.push(3);
-  queue.push(4);
+  BOOST_CHECK_EQUAL(queue.get_size(), 0);
+
+  queue.push(10);
   BOOST_CHECK(!queue.is_empty());
-  BOOST_CHECK_EQUAL(queue.pop(), 3);
-  BOOST_CHECK_EQUAL(queue.pop(), 4);
-  BOOST_CHECK(queue.is_empty());
+  BOOST_CHECK_EQUAL(queue.get_size(), 1);
+  BOOST_CHECK_EQUAL(queue.front(), 10);
+
+  queue.push(20);
+  BOOST_CHECK_EQUAL(queue.get_size(), 2);
+  BOOST_CHECK_EQUAL(queue.front(), 10);
 }
 
-BOOST_AUTO_TEST_CASE(Front)
+BOOST_AUTO_TEST_CASE(pop)
+{
+  haliullin::Queue< int > queue;
+  queue.push(10);
+  queue.push(20);
+
+  queue.pop();
+  BOOST_CHECK_EQUAL(queue.get_size(), 1);
+  BOOST_CHECK_EQUAL(queue.front(), 20);
+
+  queue.pop();
+  BOOST_CHECK(queue.is_empty());
+  BOOST_CHECK_EQUAL(queue.get_size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(front)
 {
   haliullin::Queue< int > queue;
   queue.push(42);
   BOOST_CHECK_EQUAL(queue.front(), 42);
+  queue.push(100);
+  BOOST_CHECK_EQUAL(queue.front(), 42);
   queue.pop();
-  BOOST_CHECK(queue.is_empty());
+  BOOST_CHECK_EQUAL(queue.front(), 100);
 }
 
-BOOST_AUTO_TEST_CASE(Exceptions)
+BOOST_AUTO_TEST_CASE(get_size)
 {
   haliullin::Queue< int > queue;
-  BOOST_CHECK_THROW(queue.pop(), std::runtime_error);
-  BOOST_CHECK_THROW(queue.front(), std::runtime_error);
+  BOOST_CHECK_EQUAL(queue.get_size(), 0);
+  queue.push(1);
+  queue.push(2);
+  queue.push(3);
+  BOOST_CHECK_EQUAL(queue.get_size(), 3);
+  queue.pop();
+  BOOST_CHECK_EQUAL(queue.get_size(), 2);
+  queue.clear();
+  BOOST_CHECK_EQUAL(queue.get_size(), 0);
 }
 
-BOOST_AUTO_TEST_CASE(Clear)
+BOOST_AUTO_TEST_CASE(clear)
 {
   haliullin::Queue< int > queue;
   queue.push(1);
   queue.push(2);
   queue.push(3);
+  BOOST_CHECK(!queue.is_empty());
   queue.clear();
   BOOST_CHECK(queue.is_empty());
   BOOST_CHECK_EQUAL(queue.get_size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(exceptions)
+{
+  haliullin::Queue< int > queue;
+  BOOST_CHECK_THROW(queue.pop(), std::runtime_error);
+  BOOST_CHECK_THROW(queue.front(), std::runtime_error);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- haliullin.ruslan/S2/test-stack.cpp
+++ haliullin.ruslan/S2/test-stack.cpp
@@ -4,43 +4,79 @@
 
 BOOST_AUTO_TEST_SUITE(StackTests)
 
-BOOST_AUTO_TEST_CASE(PushAndPop)
+BOOST_AUTO_TEST_CASE(push)
 {
   haliullin::Stack< int > stack;
   BOOST_CHECK(stack.is_empty());
-  stack.push(3);
-  stack.push(4);
+  BOOST_CHECK_EQUAL(stack.get_size(), 0);
+
+  stack.push(10);
   BOOST_CHECK(!stack.is_empty());
-  BOOST_CHECK_EQUAL(stack.pop(), 4);
-  BOOST_CHECK_EQUAL(stack.pop(), 3);
-  BOOST_CHECK(stack.is_empty());
+  BOOST_CHECK_EQUAL(stack.get_size(), 1);
+  BOOST_CHECK_EQUAL(stack.top(), 10);
+
+  stack.push(20);
+  BOOST_CHECK_EQUAL(stack.get_size(), 2);
+  BOOST_CHECK_EQUAL(stack.top(), 20);
 }
 
-BOOST_AUTO_TEST_CASE(Top)
+BOOST_AUTO_TEST_CASE(pop)
+{
+  haliullin::Stack< int > stack;
+  stack.push(10);
+  stack.push(20);
+
+  stack.pop();
+  BOOST_CHECK_EQUAL(stack.get_size(), 1);
+  BOOST_CHECK_EQUAL(stack.top(), 10);
+
+  stack.pop();
+  BOOST_CHECK(stack.is_empty());
+  BOOST_CHECK_EQUAL(stack.get_size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(top)
 {
   haliullin::Stack< int > stack;
   stack.push(42);
   BOOST_CHECK_EQUAL(stack.top(), 42);
+  stack.push(100);
+  BOOST_CHECK_EQUAL(stack.top(), 100);
   stack.pop();
-  BOOST_CHECK(stack.is_empty());
+  BOOST_CHECK_EQUAL(stack.top(), 42);
 }
 
-BOOST_AUTO_TEST_CASE(Exceptions)
+BOOST_AUTO_TEST_CASE(get_size)
 {
   haliullin::Stack< int > stack;
-  BOOST_CHECK_THROW(stack.pop(), std::runtime_error);
-  BOOST_CHECK_THROW(stack.top(), std::runtime_error);
+  BOOST_CHECK_EQUAL(stack.get_size(), 0);
+  stack.push(1);
+  stack.push(2);
+  stack.push(3);
+  BOOST_CHECK_EQUAL(stack.get_size(), 3);
+  stack.pop();
+  BOOST_CHECK_EQUAL(stack.get_size(), 2);
+  stack.clear();
+  BOOST_CHECK_EQUAL(stack.get_size(), 0);
 }
 
-BOOST_AUTO_TEST_CASE(Clear)
+BOOST_AUTO_TEST_CASE(clear)
 {
   haliullin::Stack< int > stack;
   stack.push(1);
   stack.push(2);
   stack.push(3);
+  BOOST_CHECK(!stack.is_empty());
   stack.clear();
   BOOST_CHECK(stack.is_empty());
   BOOST_CHECK_EQUAL(stack.get_size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(exceptions)
+{
+  haliullin::Stack< int > stack;
+  BOOST_CHECK_THROW(stack.pop(), std::runtime_error);
+  BOOST_CHECK_THROW(stack.top(), std::runtime_error);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
