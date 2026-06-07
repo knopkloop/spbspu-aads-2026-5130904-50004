af34d6f9ca23be0745a1329098fe6ca88ed45673
knopkloop <hruslan2007@gmail.com>
2026-06-08 01:12:12 +0300

    RobinHash: added slot iplementation

---
+++ haliullin.ruslan/F0/Slot.hpp
@@ -0,0 +1,28 @@
+#ifndef SLOT_HPP
+#define SLOT_HPP
+
+#include <utility>
+
+namespace haliullin
+{
+  namespace detail
+  {
+    template< class Key, class Value >
+    struct Slot
+    {
+      Slot() noexcept:
+        kv_(),
+        psl_(-1)
+      {}
+
+    private:
+      std::pair< Key, Value > kv_;
+      int psl_;
+
+      template< class K, class V, class H, class E >
+      friend class RobinHoodHashTable;
+    };
+  }
+}
+
+#endif
