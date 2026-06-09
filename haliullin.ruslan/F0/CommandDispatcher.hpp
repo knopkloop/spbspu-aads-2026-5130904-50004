6737496e570dd9bb36740039d0bb44eefdb3df6e
knopkloop <hruslan2007@gmail.com>
2026-06-09 18:39:48 +0300

    CommandDispatcher: cmd declarated (it processes commands from input and calls AppCore methods. It is model-controller of AppCore)

---
+++ haliullin.ruslan/F0/CommandDispatcher.hpp
@@ -0,0 +1,48 @@
+#ifndef COMMANDDISPATCHER_HPP
+#define COMMANDDISPATCHER_HPP
+
+#include <iosfwd>
+#include <string>
+#include "RobinHashTable.hpp"
+#include "MurMurHash.hpp"
+#include "AppCore.hpp"
+
+namespace haliullin
+{
+  class CommandDispatcher
+  {
+  public:
+    using func_t = void (CommandDispatcher::*)(std::istream&, std::ostream&);
+
+    CommandDispatcher();
+    void execute(std::istream& in, std::ostream& out);
+
+  private:
+    AppCore core_;
+    RobinHashTable< std::string, func_t, detail::MurMurHash, std::equal_to< std::string > > commands_;
+
+    void cmdCreatePhonebook(std::istream& in, std::ostream& out);
+    void cmdRemovePhonebook(std::istream& in, std::ostream& out);
+    void cmdAddContact(std::istream& in, std::ostream& out);
+    void cmdRemoveContact(std::istream& in, std::ostream& out);
+    void cmdShowBook(std::istream& in, std::ostream& out);
+    void cmdShowContact(std::istream& in, std::ostream& out);
+    void cmdMerge(std::istream& in, std::ostream& out);
+    void cmdCopyContact(std::istream& in, std::ostream& out);
+    void cmdRenameBook(std::istream& in, std::ostream& out);
+    void cmdReportSpam(std::istream& in, std::ostream& out);
+    void cmdGrade(std::istream& in, std::ostream& out);
+    void cmdDisconnect(std::istream& in, std::ostream& out);
+    void cmdShowConnections(std::istream& in, std::ostream& out);
+    void cmdRecommend(std::istream& in, std::ostream& out);
+    void cmdSave(std::istream& in, std::ostream& out);
+    void cmdLoad(std::istream& in, std::ostream& out);
+    void cmdExit(std::istream& in, std::ostream& out);
+
+    void require(bool condition) const;
+    void require(std::istream& in) const;
+    bool isNumberValid(const std::string& number) const;
+  };
+}
+
+#endif
